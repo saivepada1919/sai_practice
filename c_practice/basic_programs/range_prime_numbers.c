6717a69b1eecf51473290e5dae6461a4cca3be5e
saivepada1919 <vepadasai1919@gmail.com>
2025-09-05 15:29:21 +0530

    range of prime numbers

--- c_practice/basic_programs/range_prime_numbers.c
+++ c_practice/basic_programs/range_prime_numbers.c
@@ -1,30 +1,23 @@
 #include <stdio.h>
-#include <math.h>
 int prime(int n){
-	if(n<=1){ 
-		return 0;
-	}
-	if(n==2){
-		return 1;
-	}
-	if(n>2){
-	for(int i=3;i<=n/2;i++){
+	int count=0;
+	for(int i=2;i<=n/2;i++){
 			if(n%i==0){
-				return 0;
+				count++;
 			}
 	}
-	return 1;
-}
+	return count;
 }
 int main(){
 	int start,end;
 	printf("enter the range of values:");
 	scanf("%d%d",&start,&end);
 	for(int i=start;i<=end;i++){
-		if(prime(i)){
-			printf("%d",i);
+		if(i>1 && prime(i)==0){
+			printf("%d ",i);
 		}
 	}
+	printf("\n");
 }
 
 

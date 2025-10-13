376bc7ce9131209e7fd173c66a9de57d4ca772cd
saivepada1919 <vepadasai1919@gmail.com>
2025-10-13 23:26:52 +0530

    insert node at begining in sll

---
+++ c_practice/data_structures/insert_begining_sll.c
@@ -0,0 +1,78 @@
+#include <stdio.h>
+#include <stdlib.h>
+struct node{
+        int data;
+        struct node* link;
+};
+/*
+void insert_begining(struct node *head){
+        struct node *temp = NULL,*current = NULL;
+        temp = malloc(sizeof(struct node));
+        printf("enter the data to insert: ");
+        scanf("%d",&temp->data);
+        temp->link = head;
+        head = temp;
+
+        //after inserting element at begining
+        printf("Linked list before insering elements: ");
+        current = head;
+        while(current != NULL){
+                printf(" %d ->",current->data);
+                current = current->link;
+        }
+        printf(" NULL\n");
+}
+
+*/
+
+int main(){
+        struct node *head = NULL,*current = NULL,*temp = NULL;
+        int n;
+        printf("enter the no of nodes: ");
+        scanf("%d",&n);
+        for(int i=1;i<=n;i++){
+                temp = malloc(sizeof(struct node));
+                printf("enter the data for node %d: ",i);
+                scanf("%d",&temp->data);
+                temp->link = NULL;
+
+                if(head == NULL){
+                        head = temp;
+                        current = temp;
+                }
+                else{
+                        current->link = temp;
+                        current = temp;
+                }
+        }
+        //before inserting elements
+        printf("Linked list before insering elements: ");
+        current = head;
+        while(current != NULL){
+                printf(" %d ->",current->data);
+                current = current->link;
+        }
+        printf(" NULL\n");
+
+     //   insert_begining(head);
+
+
+        //insert element at begining
+        temp = malloc(sizeof(struct node));
+        printf("enter the data to insert: ");
+        scanf("%d",&temp->data);
+        temp->link = head;
+        head = temp;
+
+        //after inserting element at begining
+        printf("Linked list before insering elements: ");
+        current = head;
+        while(current != NULL){
+                printf(" %d ->",current->data);
+                current = current->link;
+        }
+        printf(" NULL\n");
+
+        return 0;
+}
+

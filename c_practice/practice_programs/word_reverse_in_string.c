f27c37b58da419f70ebe7e773d91e71e797e9603
saivepada1919 <vepadasai1919@gmail.com>
2025-12-03 18:23:50 +0530

    word reverse in sentence

---
+++ c_practice/practice_programs/word_reverse_in_string.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <string.h>
+void reverse(char *s,char *e){
+	while(s<e){
+		char temp = *s;
+		*s = *e;
+		*e = temp;
+		s++;
+		e--;
+	}
+}
+int main(){
+	char str[100];
+	printf("enter string : ");
+	fgets(str,100,stdin);
+	str[strcspn(str,"\n")] = '\0';
+	char *s = str;
+	char *e = str;
+	int i = 0;
+	while(*e){
+		if(str[i] == ' ' || str[i] == '\t'){
+			reverse(s,e-1);
+			s = e+1;
+		}
+		e++;
+		i++;
+	}
+	reverse(s,e-1);
+	printf("reverse string : %s\n",str);
+	return 0;
+}
+	
+
